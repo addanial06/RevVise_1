91ea78ecee2d928a071b2bad7bec182ca3db1d31
5000adel <XSrillixon@gmail.com>
2025-11-11 04:25:51 +0800

    reverted item.cs

--- RevVise1/Forms/Views/Item.Designer.cs
+++ RevVise1/Forms/Views/Item.Designer.cs
@@ -28,180 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            IDLabel = new Label();
             modelLabel = new Label();
-            plateLabel = new Label();
-            modelTB = new TextBox();
-            plateTB = new TextBox();
-            ownerTB = new TextBox();
-            contactTB = new TextBox();
-            ownerLabel = new Label();
             contactLabel = new Label();
-            maintenanceLabel = new Label();
-            maintenanceTB = new TextBox();
-            remarkLabel = new Label();
-            remarksTB = new TextBox();
-            addBtn = new Button();
-            updateBtn = new Button();
+            vehicleLabel = new Label();
+            editButton = new Button();
+            modelText = new TextBox();
+            contactText = new TextBox();
+            vehicleText = new TextBox();
+            entryText = new RichTextBox();
+            entryLabel = new Label();
             SuspendLayout();
+            // 
+            // IDLabel
+            // 
+            IDLabel.AutoSize = true;
+            IDLabel.Location = new Point(19, 10);
+            IDLabel.Margin = new Padding(2, 0, 2, 0);
+            IDLabel.Name = "IDLabel";
+            IDLabel.Size = new Size(25, 21);
+            IDLabel.TabIndex = 0;
+            IDLabel.Text = "ID";
             // 
             // modelLabel
             // 
             modelLabel.AutoSize = true;
-            modelLabel.Location = new Point(14, 13);
+            modelLabel.Location = new Point(60, 10);
             modelLabel.Margin = new Padding(2, 0, 2, 0);
             modelLabel.Name = "modelLabel";
             modelLabel.Size = new Size(54, 21);
             modelLabel.TabIndex = 1;
             modelLabel.Text = "Model";
             // 
-            // plateLabel
-            // 
-            plateLabel.AutoSize = true;
-            plateLabel.Location = new Point(14, 48);
-            plateLabel.Name = "plateLabel";
-            plateLabel.Size = new Size(72, 21);
-            plateLabel.TabIndex = 3;
-            plateLabel.Text = "Plate No.";
-            // 
-            // modelTB
-            // 
-            modelTB.Location = new Point(115, 10);
-            modelTB.Name = "modelTB";
-            modelTB.Size = new Size(296, 29);
-            modelTB.TabIndex = 4;
-            // 
-            // plateTB
-            // 
-            plateTB.Location = new Point(115, 45);
-            plateTB.Name = "plateTB";
-            plateTB.Size = new Size(296, 29);
-            plateTB.TabIndex = 5;
-            // 
-            // ownerTB
-            // 
-            ownerTB.Location = new Point(115, 80);
-            ownerTB.Name = "ownerTB";
-            ownerTB.Size = new Size(296, 29);
-            ownerTB.TabIndex = 6;
-            // 
-            // contactTB
-            // 
-            contactTB.Location = new Point(115, 115);
-            contactTB.Name = "contactTB";
-            contactTB.Size = new Size(296, 29);
-            contactTB.TabIndex = 9;
-            // 
-            // ownerLabel
-            // 
-            ownerLabel.AutoSize = true;
-            ownerLabel.Location = new Point(14, 83);
-            ownerLabel.Name = "ownerLabel";
-            ownerLabel.Size = new Size(57, 21);
-            ownerLabel.TabIndex = 10;
-            ownerLabel.Text = "Owner";
-            // 
             // contactLabel
             // 
             contactLabel.AutoSize = true;
-            contactLabel.Location = new Point(14, 118);
+            contactLabel.Location = new Point(200, 10);
+            contactLabel.Margin = new Padding(2, 0, 2, 0);
             contactLabel.Name = "contactLabel";
-            contactLabel.Size = new Size(91, 21);
-            contactLabel.TabIndex = 11;
-            contactLabel.Text = "Contact No.";
+            contactLabel.Size = new Size(63, 21);
+            contactLabel.TabIndex = 2;
+            contactLabel.Text = "Contact";
             // 
-            // maintenanceLabel
+            // vehicleLabel
             // 
-            maintenanceLabel.AutoSize = true;
-            maintenanceLabel.Location = new Point(463, 13);
-            maintenanceLabel.Margin = new Padding(2, 0, 2, 0);
-            maintenanceLabel.Name = "maintenanceLabel";
-            maintenanceLabel.Size = new Size(139, 21);
-            maintenanceLabel.TabIndex = 12;
-            maintenanceLabel.Text = "Maintenance Entry";
+            vehicleLabel.AutoSize = true;
+            vehicleLabel.Location = new Point(400, 10);
+            vehicleLabel.Name = "vehicleLabel";
+            vehicleLabel.Size = new Size(99, 21);
+            vehicleLabel.TabIndex = 3;
+            vehicleLabel.Text = "Vehicle Desc.";
             // 
-            // maintenanceTB
+            // editButton
             // 
-            maintenanceTB.Location = new Point(607, 10);
-            maintenanceTB.Name = "maintenanceTB";
-            maintenanceTB.Size = new Size(296, 29);
-            maintenanceTB.TabIndex = 13;
+            editButton.Location = new Point(819, 8);
+            editButton.Name = "editButton";
+            editButton.Size = new Size(75, 30);
+            editButton.TabIndex = 4;
+            editButton.Text = "Edit";
+            editButton.UseVisualStyleBackColor = true;
+            editButton.Click += Edit_Click;
             // 
-            // remarkLabel
+            // modelText
             // 
-            remarkLabel.AutoSize = true;
-            remarkLabel.Location = new Point(463, 53);
-            remarkLabel.Margin = new Padding(2, 0, 2, 0);
-            remarkLabel.Name = "remarkLabel";
-            remarkLabel.Size = new Size(71, 21);
-            remarkLabel.TabIndex = 14;
-            remarkLabel.Text = "Remarks";
+            modelText.BackColor = SystemColors.InactiveCaption;
+            modelText.BorderStyle = BorderStyle.FixedSingle;
+            modelText.Location = new Point(60, 7);
+            modelText.Name = "modelText";
+            modelText.Size = new Size(100, 29);
+            modelText.TabIndex = 5;
+            modelText.Visible = false;
             // 
-            // remarksTB
+            // contactText
             // 
-            remarksTB.Location = new Point(607, 45);
-            remarksTB.Name = "remarksTB";
-            remarksTB.Size = new Size(296, 29);
-            remarksTB.TabIndex = 15;
+            contactText.BackColor = SystemColors.InactiveCaption;
+            contactText.BorderStyle = BorderStyle.FixedSingle;
+            contactText.Location = new Point(200, 7);
+            contactText.Name = "contactText";
+            contactText.Size = new Size(100, 29);
+            contactText.TabIndex = 6;
+            contactText.Visible = false;
             // 
-            // addBtn
+            // vehicleText
             // 
-            addBtn.Location = new Point(467, 118);
-            addBtn.Name = "addBtn";
-            addBtn.Size = new Size(75, 29);
-            addBtn.TabIndex = 16;
-            addBtn.Text = "Add";
-            addBtn.UseVisualStyleBackColor = true;
+            vehicleText.BackColor = SystemColors.InactiveCaption;
+            vehicleText.BorderStyle = BorderStyle.FixedSingle;
+            vehicleText.Location = new Point(400, 7);
+            vehicleText.Name = "vehicleText";
+            vehicleText.Size = new Size(100, 29);
+            vehicleText.TabIndex = 7;
+            vehicleText.Visible = false;
             // 
-            // updateBtn
+            // entryText
             // 
-            updateBtn.Location = new Point(548, 118);
-            updateBtn.Name = "updateBtn";
-            updateBtn.Size = new Size(75, 29);
-            updateBtn.TabIndex = 17;
-            updateBtn.Text = "Update";
-            updateBtn.UseVisualStyleBackColor = true;
+            entryText.BackColor = SystemColors.InactiveCaption;
+            entryText.BorderStyle = BorderStyle.FixedSingle;
+            entryText.Location = new Point(580, 8);
+            entryText.Name = "entryText";
+            entryText.Size = new Size(500, 120);
+            entryText.TabIndex = 8;
+            entryText.Text = "";
+            entryText.Visible = false;
+            // 
+            // entryLabel
+            // 
+            entryLabel.AutoSize = true;
+            entryLabel.Location = new Point(591, 13);
+            entryLabel.MaximumSize = new Size(500, 120);
+            entryLabel.Name = "entryLabel";
+            entryLabel.Size = new Size(227, 21);
+            entryLabel.TabIndex = 9;
+            entryLabel.Text = "entry 1a sdasd asd asd asd asd ";
+            entryLabel.Visible = false;
             // 
             // Item
             // 
             AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.White;
-            ClientSize = new Size(1075, 157);
-            Controls.Add(updateBtn);
-            Controls.Add(addBtn);
-            Controls.Add(remarksTB);
-            Controls.Add(remarkLabel);
-            Controls.Add(maintenanceTB);
-            Controls.Add(maintenanceLabel);
+            BackColor = SystemColors.ActiveCaption;
+            Controls.Add(entryLabel);
+            Controls.Add(entryText);
+            Controls.Add(vehicleText);
+            Controls.Add(contactText);
+            Controls.Add(modelText);
+            Controls.Add(editButton);
+            Controls.Add(vehicleLabel);
             Controls.Add(contactLabel);
-            Controls.Add(ownerLabel);
-            Controls.Add(contactTB);
-            Controls.Add(ownerTB);
-            Controls.Add(plateTB);
-            Controls.Add(modelTB);
-            Controls.Add(plateLabel);
             Controls.Add(modelLabel);
+            Controls.Add(IDLabel);
             Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Margin = new Padding(10);
+            Margin = new Padding(4);
             Name = "Item";
+            Size = new Size(900, 43);
+            Click += Item_Click;
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
+
+        private Label IDLabel;
         private Label modelLabel;
-        private Label plateLabel;
-        private TextBox modelTB;
-        private TextBox plateTB;
-        private TextBox ownerTB;
-        private TextBox contactTB;
-        private Label ownerLabel;
         private Label contactLabel;
-        private Label maintenanceLabel;
-        private TextBox maintenanceTB;
-        private Label remarkLabel;
-        private TextBox remarksTB;
-        private Button addBtn;
-        private Button updateBtn;
+        private Label vehicleLabel;
+        private Button editButton;
+        private TextBox modelText;
+        private TextBox contactText;
+        private TextBox vehicleText;
+        private RichTextBox entryText;
+        private Label entryLabel;
     }
 }
